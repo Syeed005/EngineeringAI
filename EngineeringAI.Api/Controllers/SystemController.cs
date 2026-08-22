47c078faa93565b0106755f381cd5974d5fea3b0
Md Syeedur Rahman <syeed_rahman@live.com>
2026-08-22 02:17:39 -0500

    Add system version endpoint

---
+++ EngineeringAI.Api/Controllers/SystemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EngineeringAI.Api.Controllers {
+    [ApiController]
+    [Route("api/system")]
+    public class SystemController : ControllerBase {
+        [HttpGet("version")]
+        public IActionResult GetVersion() {
+            return Ok(new {
+                application = "EngineeringAI",
+                version = "1.0.1",
+                environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"
+            });
+        }
+    }
+}
